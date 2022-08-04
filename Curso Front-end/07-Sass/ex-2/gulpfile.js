f9c79366fd572f0acaf00c95148e6fb504a47ecb
vmxMoretti <vmbxmoretti@gmail.com>
2022-08-04 10:03:48 -0300

    sass 2

---
+++ Curso Front-end/07-Sass/ex-2/gulpfile.js
@@ -0,0 +1,89 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const image = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const { series, parallel } = require('gulp');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload
+
+
+function tarefasCSS(cb) {
+
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './vendor/owl/css/owl.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './src/css/style.css'
+    ])       
+        .pipe(concat('styles.css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/css'))
+}
+function tarefasJS() {
+
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './src/js/custom.js'
+    ])
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function tarefasIMAGE() {
+    
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true 
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
+function tarefasHTML(callback) {
+    
+    gulp.src('*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+}
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+    
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload)
+   
+})
+
+const process = series(tarefasHTML, tarefasCSS, tarefasJS)
+
+
+exports.imagem = tarefasIMAGE
+exports.default = process
+
---
+++ Curso Front-end/07-Sass/ex-2/src/js/custom.js
@@ -0,0 +1,124 @@
+// instancia jquery e evita conflitos
+// jQuery( function($){
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
+    let titulos = $('h4') // tag
+   
+    let itens = $('.featured-item') // class
+    
+    let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+    // Configuração de produtos
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').addClass('active')
+    // $('.featured-item:first h4').removeClass('active')
+    // $('.featured-item:first h4').toggleClass('active')
+    // $('.featured-item:first h4').hide()
+    // $('.featured-item:first h4').show()
+    // $('.featured-item:first h4').fadeIn(2000)
+    // $('.featured-item:first h4').fadeOut()
+    //  $('.featured-item:first h4').css('color', '#f00')
+     
+     $('.featured-item h4').dblclick( function(){
+
+        $(this).css({
+            'color': '#f00',
+            'background': '#ff0',
+            'font-weight': '100',
+        });
+
+     });
+
+     /*
+      * Manipulação de eventos
+      */
+     $('.featured-item a').on('blur', function(event){
+
+        event.preventDefault();
+
+        alert('Produto esgotado');
+
+     })
+     
+   $('.nav-modal-open').on('click', function(e){
+      e.preventDefault();
+      
+      let elem = $(this).attr('rel');
+
+      $('.modal-body').html($('#'+elem).html());
+      $('.modal-header h5.modal-title').html($(this).text())
+
+      let myModal = new bootstrap.Modal($('#modelId'));
+
+      myModal.show();
+   })     
+     
+   
+   $('body').on('click', '.modal-body .form', function (e){
+      e.preventDefault();
+      const inputEmail = $('#email')
+      
+      if(inputEmail.hasClass('invalid') || inputEmail.val() == ""){
+         console.log('invalid')
+         return false
+      } else {
+         console.log('success')
+         $(this).submit()
+         
+      }
+      
+
+   })
+
+   $('body').on('blur', '#email', function(){
+      validate($(this))
+      
+   })
+
+   $('body').on('blur', '#cpf', function(){
+      $('#cpf').mask('000.000.000-00', {reverse: true});
+      validate($(this))
+      
+   })
+
+   $('body').on('blur', '#cep', function(){
+      $('#cep').mask('00000-000');
+      validate($(this))
+   })
+   
+   $('body').on('blur', '#telefone', function(){
+      $('#telefone').mask('00000-0000');
+      validate($(this))
+      
+   })
+   
+   
+   
+  
+   
+   
+   function validate(validar){
+      if (validar.val() == "") {
+         console.log(validar.attr("name")+' obrigatorio')
+         validar.addClass('invalid')
+         validar.parent().find('.text-muted').show()
+
+         return false
+      } else {
+         validar.removeClass('invalid')
+         validar.parent().find('.text-muted').hide()
+         return true
+      }
+   }
+   
+   
+})
